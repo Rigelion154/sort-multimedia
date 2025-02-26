d829e5a665234ede6dc60329f27cae70d19bddd0
Павел Иванов <crmprog3@mtkrosberg.ru>
2025-02-26 17:25:03 +0300

    feat: add extensions and cors

--- src/routes/sort.ts
+++ src/routes/sort.ts
@@ -3,5 +3,5 @@ import {MultimediaSorter} from "../sort-multimedia/MultimediaSorter";
 
 export const sortRouter = express.Router()
 
-sortRouter.post('/src', (req, res) => MultimediaSorter.setPath(req, res, 'source'))
-sortRouter.post('/target', (req, res) => MultimediaSorter.setPath(req, res, 'target'))
+sortRouter.post('/path', MultimediaSorter.setPath)
+sortRouter.post('/extension', MultimediaSorter.addExtension)
--- src/sort-multimedia/MultimediaSorter.ts
+++ src/sort-multimedia/MultimediaSorter.ts
@@ -4,12 +4,19 @@ import {readdir} from "node:fs/promises";
 class MultimediaSorterClient {
     sourcePath: string | null = null;
     targetPath: string | null = null;
+    photoExtensions: string[] = ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'webp'];
+    videoExtensions: string[] = ['mp4', 'mov', 'avi', 'mkv', 'webm'];
 
     constructor() {
     }
 
-    setPath = async (req: Request, res: Response, type: 'source' | 'target') => {
-        const { newPath } = req.body
+    setPath = async (req: Request, res: Response) => {
+        const {newPath, type} = req.body
+
+        if (!type) {
+            res.status(404).send({hasErrors: true, message: 'Укажите тип пути'})
+            return
+        }
 
         if (type === 'source') {
             this.sourcePath = newPath
@@ -21,11 +28,28 @@ class MultimediaSorterClient {
 
         try {
             const items = await readdir(newPath)
-
-            res.status(200).send({data: {newPath, items}, hasErrors: false})
-        } catch (e) {
+            res.status(200).send({hasErrors: false, data: {newPath, items}})
+        } catch (error) {
             res.status(404).send({hasErrors: true, message: 'Указанный путь не существует'})
         }
+    }
+
+    addExtension = async (req: Request, res: Response) => {
+        const {extension, type} = req.body
+
+        if (type === 'video') {
+            this.videoExtensions.push(extension)
+            res.send({hasErrors: false, data: {extensions: this.videoExtensions}})
+            return
+        }
+
+        if (type === 'photo') {
+            this.photoExtensions.push(extension)
+            res.send({hasErrors: false, data: {extensions: this.photoExtensions}})
+            return
+        }
+
+        res.status(404).send({hasErrors: true, message: 'Ошибка при добавлении расширения'})
     }
 
 }
