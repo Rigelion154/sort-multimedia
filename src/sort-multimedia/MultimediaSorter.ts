514e3542bfeb8adbb2ce34a4c1d2c3eff4a95f34
Rigelion154 <cupofbeer123@gmail.com>
2025-02-27 23:18:22 +0300

    refactor: some tests

--- src/sort-multimedia/MultimediaSorter.ts
+++ src/sort-multimedia/MultimediaSorter.ts
@@ -1,5 +1,8 @@
 import {Request, Response} from 'express'
-import {readdir} from "node:fs/promises";
+import {readdir, stat} from "node:fs/promises";
+import {join} from 'node:path';
+import * as os from "node:os";
+
 
 class MultimediaSorterClient {
     sourcePath: string | null = null;
@@ -11,7 +14,7 @@ class MultimediaSorterClient {
     }
 
     setPath = async (req: Request, res: Response) => {
-        const {newPath, type} = req.body
+        const {folderPath, type} = req.body
 
         if (!type) {
             res.status(404).send({hasErrors: true, message: 'Укажите тип пути'})
@@ -19,16 +22,53 @@ class MultimediaSorterClient {
         }
 
         if (type === 'source') {
-            this.sourcePath = newPath
+            this.sourcePath = folderPath
         }
 
         if (type === 'target') {
-            this.targetPath = newPath
+            this.targetPath = folderPath
         }
 
         try {
-            const items = await readdir(newPath)
-            res.status(200).send({hasErrors: false, data: {newPath, items}})
+            const folderFiles = await readdir(folderPath)
+            const resultStatList = []
+
+            for (const file of folderFiles) {
+                const filePath = join(folderPath, file)
+                const extension = file.split('.').pop()?.toLowerCase();
+                const isImage = this.photoExtensions.includes(extension ?? '');
+                const isVideo = this.videoExtensions.includes(extension ?? '');
+
+                try {
+                    const fileStat = await stat(filePath)
+                    const filetype = fileStat.isDirectory() ? 'directory' : isImage ? 'image' : isVideo ? 'video' : 'file'
+
+                    resultStatList.push({
+                        fileName: file,
+                        isDirectory: fileStat.isDirectory(),
+                        createdAt: fileStat.mtime,
+                        isLocked: false,
+                        filetype
+                    })
+                } catch (error) {
+                    resultStatList.push({
+                        fileName: file,
+                        isLocked: true,
+                        filetype: 'warning'
+                    })
+                    console.error(error)
+                }
+
+            }
+
+            const filesStatList = resultStatList.sort((a, b) => {
+                if (a.isDirectory === b.isDirectory) {
+                    return 0;
+                }
+                return a.isDirectory ? -1 : 1;
+            })
+
+            res.status(200).send({hasErrors: false, folderPath, filesStatList})
         } catch (error) {
             res.status(404).send({hasErrors: true, message: 'Указанный путь не существует'})
         }
