a0a68f489e748ccfa6e30a5ec6a654dbb4319a5c
Rigelion154 <cupofbeer123@gmail.com>
2025-03-14 23:48:27 +0300

    feat: add open lib

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors'
 import {sortRouter} from "./routes/sort";
+import open from 'open';
 
 const PORT = process.env.PORT || 8535;
 
@@ -9,8 +10,9 @@ const runApp = () => {
     app.use(express.json())
     app.use(cors())
     app.use('/api/v1/sort', sortRouter)
-    app.listen(PORT, () => {
+    app.listen(PORT, async () => {
         console.log(`Server is running on http://localhost:${PORT}`);
+        await open('https://multimedia-sorter.netlify.app/');
     });
 }
 
