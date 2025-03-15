6db9f1ac2c2de0fe5f20f3f2a57bb81a94ac74d0
Rigelion154 <cupofbeer123@gmail.com>
2025-03-15 13:35:51 +0300

    feat: add bat file to start server

--- start.bat
+++ start.bat
@@ -1,3 +1,35 @@
 @echo off
-npm install
-npm start
+echo Checking for Node.js...
+
+REM Проверка наличия Node.js
+node -v >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Node.js is not installed.
+    echo Please install Node.js from https://nodejs.org and try again.
+    pause
+    exit /b
+)
+
+echo Node.js is installed. Proceeding with setup...
+
+echo setup modules...
+call npm install
+if %errorlevel% neq 0 (
+    echo Error during npm install. Exiting...
+    pause
+    exit /b
+)
+
+echo setup modules complete.
+
+
+echo start server...
+call node dist/index.js
+if %errorlevel% neq 0 (
+    echo Failed to start server. Check the code.
+    pause
+    exit /b
+)
+
+echo Server started successfully. Running at http://localhost:8535
+pause
